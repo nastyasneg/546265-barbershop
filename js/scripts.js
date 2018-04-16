5ed7b8e46140ac10945fa925fcc143e53a3e4b93
Nastya Sneg <sneg.bz@gmail.com>
2018-04-16 12:48:34 +0300

    Модальное окно с картой

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,26 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
+var mapLink = document.querySelector(".contacts-button-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-map-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-map-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (mapPopup.classList.contains("modal-map-show")) {
+      mapPopup.classList.remove("modal-map-show");
+    }
+  }
+});
